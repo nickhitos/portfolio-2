614b22277f39f2f3a0875ae6b8f6c5f1e84661a9
nickhitos <nickhit3802@gmail.com>
2024-08-20 14:46:45 -0700

    disable contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,21 +31,21 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "Nicolas Hitosis",
-          from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
+    // emailjs
+    //   .send(
+    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    //     {
+    //       from_name: form.name,
+    //       to_name: "Nicolas Hitosis",
+    //       from_email: form.email,
+    //       to_email: "sujata@jsmastery.pro",
+    //       message: form.message,
+    //     },
+    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    //   )
+    //   .then(
+    //     () => {
           setLoading(false);
           alert("Thank you. I will get back to you as soon as possible.");
 
@@ -54,14 +54,14 @@ const Contact = () => {
             email: "",
             message: "",
           });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
+      //   },
+      //   (error) => {
+      //     setLoading(false);
+      //     console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+      //     alert("Ahh, something went wrong. Please try again.");
+      //   }
+      // );
   };
 
   return (
